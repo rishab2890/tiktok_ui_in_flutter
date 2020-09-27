7cd80e01da1adcd5fb695b2341c688fab40496ee
rishab2890 <rishabsharma2890@gmail.com>
2020-09-27 10:39:31 +0530

    basic layout completed

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,81 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
+  Widget get topPart => Container(
+        height: 100.0,
+        padding: EdgeInsets.only(bottom: 15.0),
+        color: Colors.yellow,
+      );
+
+  Widget get middlePart => Expanded(
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            videoDescription,
+            actionsToolbar,
+          ],
+        ),
+      );
+
+  Widget get bottomPart => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: List<Widget>.generate(
+          5,
+          (index) => Container(
+            width: 40.0,
+            height: 40.0,
+            color: Colors.purple[300],
+          ),
+        ),
+      );
+
+  Widget get videoDescription => Expanded(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              height: 10.0,
+              color: Colors.green[300],
+              margin: EdgeInsets.only(top: 10)),
+          Container(
+              height: 10.0,
+              color: Colors.green[300],
+              margin: EdgeInsets.only(top: 10)),
+          Container(
+              height: 10.0,
+              color: Colors.green[300],
+              margin: EdgeInsets.only(top: 10))
+        ],
+      ));
+
+  Widget get actionsToolbar => Container(
+        width: 100.0,
+        color: Colors.red[300],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: List<Widget>.generate(
+              5,
+              (index) => Container(
+                    width: 60.0,
+                    height: 60.0,
+                    color: Colors.blue[300],
+                    margin: EdgeInsets.only(top: 20.0),
+                  )),
+        ),
+      );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           //top part
-          Container(
-            height: 100.0,
-            padding: EdgeInsets.only(bottom: 15.0),
-            color: Colors.yellow,
-          ),
+          topPart,
           //middle part
-          Expanded(
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.green[300],
-                  ),
-                ),
-                Container(
-                  width: 100.0,
-                  color: Colors.red[300],
-                ),
-              ],
-            ),
-          ),
+          middlePart,
           //bottom part
-          Container(
-            height: 80.0,
-            color: Colors.blue[300],
-          )
+          bottomPart,
         ],
       ),
     );
